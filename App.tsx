c034eae53b9b3fbead5f10f8642f881d8ad9bc4d
Divya <tambat.divya06@gmail.com>
2024-12-21 16:43:55 +0530

    react native frontend code

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import Home from './src/screens/Home';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <Home />
+    </View>
+  );
+};
+
+export default App;
